f6db71e0ba0ba1184f050a53262f516eccb53a7e
Michele Zanotti <m.zanotti019@gmail.com>
2023-03-03 17:28:34 +0100

    Implement submit experiment CLI command

--- apps/accelerate/cloud_surfer/tests/core/test_schemas.py
+++ apps/accelerate/cloud_surfer/tests/core/test_schemas.py
@@ -3,8 +3,9 @@ from pathlib import Path
 from tempfile import TemporaryDirectory
 
 import yaml
+from pydantic.error_wrappers import ValidationError
 
-from surfer.core.schemas import SurferConfig
+from surfer.core.schemas import SurferConfig, ExperimentConfig
 from tests.core.test_services import MockedStorageConfig
 
 
@@ -47,3 +48,31 @@ class TestSurferConfig(unittest.TestCase):
             cluster_file=valid_yaml_path,
             storage=MockedStorageConfig(),
         )
+
+
+class TestExperimentConfig(unittest.TestCase):
+    def test_validation__paths_must_resolve_to_file(self):
+        with TemporaryDirectory() as tmp_dir:
+            with self.assertRaises(ValidationError):
+                ExperimentConfig(
+                    description="test",
+                    data_loader_module=Path(tmp_dir),
+                    model_loader_module=Path(tmp_dir),
+                    model_evaluator_module=Path(tmp_dir),
+                )
+
+    def test_yaml_serialization_deserialization(self):
+        with TemporaryDirectory() as tmp_dir:
+            tmp_file_path = Path(tmp_dir) / "tmp.py"
+            with open(tmp_file_path, "w") as f:
+                f.write("")
+            config = ExperimentConfig(
+                description="test",
+                data_loader_module=tmp_file_path,
+                model_loader_module=tmp_file_path,
+                model_evaluator_module=tmp_file_path,
+            )
+            deserialized_yaml = yaml.safe_load(yaml.dump(config.dict()))
+            deserialized_config = ExperimentConfig(**deserialized_yaml)
+            self.assertEqual(config.dict(), deserialized_config.dict())
+
