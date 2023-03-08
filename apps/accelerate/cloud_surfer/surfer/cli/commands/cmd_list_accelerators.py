3f0198c5ef112f3121ba48c2cb40c7cbca54d654
Michele Zanotti <m.zanotti019@gmail.com>
2023-03-08 16:41:50 +0100

    Add list accelerators command, refactoring

---
+++ apps/accelerate/cloud_surfer/surfer/cli/commands/cmd_list_accelerators.py
@@ -0,0 +1,20 @@
+from rich import print
+from rich.table import Table
+
+from surfer.cli.commands.common import must_load_config
+from surfer.core import clusters
+
+
+def list_accelerators():
+    surfer_config = must_load_config()
+    accelerators = clusters.get_available_accelerators(surfer_config)
+    table = Table(title="Available Accelerators")
+    table.add_column("Code", style="cyan")
+    table.add_column("Description", style="cyan")
+    for a in accelerators:
+        table.add_row(a.value, a.display_name)
+    print(table)
+    if len(accelerators) == 0:
+        print("No accelerators found in cluster at {}".format(
+            surfer_config.cluster_file.as_posix()
+        ))
--- apps/accelerate/cloud_surfer/surfer/cli/surfer.py
+++ apps/accelerate/cloud_surfer/surfer/cli/surfer.py
@@ -1,15 +1,12 @@
 from pathlib import Path
 
 import typer as typer
-from rich.prompt import Confirm, Prompt
 
-from surfer import storage
+from surfer.cli.commands import cmd_init
+from surfer.cli.commands import cmd_list_accelerators
 from surfer.cli.experiments import app as experiment_app
 from surfer.common import constants
-from surfer.common.schemas import SurferConfig
-from surfer.core.config import SurferConfigManager
-from surfer.log import logger
-from surfer.storage.models import StorageProvider, StorageConfig
+from surfer.storage.models import StorageProvider
 
 app = typer.Typer(no_args_is_help=True)
 app.add_typer(
@@ -18,49 +15,6 @@ app.add_typer(
     name="experiment",
     help="Manage model optimization experiments",
 )
-
-
-def _new_azure_storage_config() -> StorageConfig:
-    if StorageProvider.AZURE not in storage.enabled_providers:
-        logger.error(
-            "Azure storage is not enabled. "
-            "Please install surfer[azure] to use Azure as storage provider"
-        )
-        raise typer.Exit(1)
-
-    from surfer.storage.providers import azure
-
-    sas_url = azure.URLPrompt.ask(
-        "Please enter a Storage Container SAS URL",
-    )
-    return azure.AzureStorageConfig(sas_url=sas_url)
-
-
-def _new_gcp_storage_config() -> StorageConfig:
-    if StorageProvider.GCP not in storage.enabled_providers:
-        logger.error(
-            "GCP storage is not enabled. "
-            "Please install surfer[gcp] to use GCP as storage provider"
-        )
-        raise typer.Exit(1)
-
-    from surfer.storage.providers import gcp
-
-    bucket = Prompt.ask("Insert bucket name")
-    project = Prompt.ask("Insert project name")
-    return gcp.GCPStorageConfig(bucket=bucket, project=project)
-
-
-def _new_aws_storage_config():
-    if StorageProvider.AWS not in storage.enabled_providers:
-        logger.error(
-            "AWS storage is not enabled. "
-            "Please install surfer[aws] to use AWS as storage provider"
-        )
-        raise typer.Exit(1)
-    from surfer.storage.providers import aws
-
-    return aws.AWSStorageConfig()
 
 
 @app.command(
@@ -87,35 +41,15 @@ def init(
         help="Address of the head node of the Ray cluster",
     ),
 ):
-    # Init storage config
-    storage_config = None
-    if storage_provider is StorageProvider.AZURE:
-        storage_config = _new_azure_storage_config()
-    if storage_provider is StorageProvider.AWS:
-        storage_config = _new_aws_storage_config()
-    if storage_provider is StorageProvider.GCP:
-        storage_config = _new_gcp_storage_config()
+    cmd_init.init(cluster_file, storage_provider, ray_address)
 
-    # Save config
-    config_manager = SurferConfigManager()
-    write_config = True
-    if config_manager.config_exists():
-        write_config = Confirm.ask(
-            "Found existing CloudSurfer configuration. Do you want to overwrite it?",
-            default=False,
-            show_choices=True,
-        )
-    if write_config:
-        config = SurferConfig(
-            cluster_file=cluster_file,
-            storage=storage_config,
-            ray_address=ray_address,
-        )
-        config_manager.save_config(config)
-        logger.info(
-            "Cloud Surfer configuration initialized",
-            config_manager.load_config(),
-        )
+
+@app.command(
+    name="list-accelerators",
+    help="List the accelerators available in the configured cluster.",
+)
+def list_accelerators():
+    cmd_list_accelerators.list_accelerators()
 
 
 def main():
