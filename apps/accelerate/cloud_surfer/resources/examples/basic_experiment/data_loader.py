7c9d665e01afac392a160bff476746698fbe28c1
Michele Zanotti <m.zanotti019@gmail.com>
2023-03-09 19:10:39 +0100

    Fix sample experiment

--- apps/accelerate/cloud_surfer/resources/examples/basic_experiment/data_loader.py
+++ apps/accelerate/cloud_surfer/resources/examples/basic_experiment/data_loader.py
@@ -1,8 +1,10 @@
 from typing import Iterable
 
+import torch
+
 from surfer import DataLoader
 
 
 class BasicDataLoader(DataLoader):
     def load_data(self, *args, **kwargs) -> Iterable:
-        return []
+        return [((torch.randn(1, 3, 256, 256),), 0) for _ in range(100)]
--- apps/accelerate/cloud_surfer/resources/examples/basic_experiment/model_loader.py
+++ apps/accelerate/cloud_surfer/resources/examples/basic_experiment/model_loader.py
@@ -1,6 +1,8 @@
+import torchvision.models as models
+
 from surfer import ModelLoader
 
 
 class BasicModelLoader(ModelLoader):
     def load_model(self, *args, **kwargs) -> any:
-        return None
+        return models.resnet50()
