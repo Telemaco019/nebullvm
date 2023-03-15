9b9b3b9abccd84300ab3eb0e29d23ffda0813e7e
Michele Zanotti <m.zanotti019@gmail.com>
2023-03-15 12:27:01 +0100

    Improve list accelerators output format

--- apps/accelerate/cloud_surfer/surfer/cli/commands/cmd_list_accelerators.py
+++ apps/accelerate/cloud_surfer/surfer/cli/commands/cmd_list_accelerators.py
@@ -1,3 +1,4 @@
+from rich import box
 from rich import print
 from rich.table import Table
 
@@ -8,7 +9,7 @@ from surfer.computing import clusters
 def list_accelerators():
     surfer_config = must_load_config()
     accelerators = clusters.get_available_accelerators(surfer_config)
-    table = Table(title="Available Accelerators")
+    table = Table(box=box.SIMPLE, expand=False)
     table.add_column("Code", style="cyan")
     table.add_column("Description", style="cyan")
     for a in accelerators:
