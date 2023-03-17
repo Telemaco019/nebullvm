d382cf7829ab87733b322dac3bd0b5e34fab4fa5
Michele Zanotti <m.zanotti019@gmail.com>
2023-03-17 10:42:43 +0100

    Test init config commands

--- apps/accelerate/cloud_surfer/tests/cli/test_surfer.py
+++ apps/accelerate/cloud_surfer/tests/cli/test_surfer.py
@@ -26,4 +26,129 @@ class TestListAccelerators(unittest.TestCase):
     def test_no_accelerators_found(self, mock_get_available_accelerators, _):
         mock_get_available_accelerators.return_value = []
         result = runner.invoke(app, ["list-accelerators"])
-        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(result.exit_code, 0, result.stdout)
+
+
+class TestInitSurferConfigCli(unittest.TestCase):
+    def test_storage_provider_azure__invalid_url(self):
+        with NamedTemporaryFile() as tmp:
+            # Run command
+            args = [
+                "init",
+                tmp.name,
+                "--storage-provider",
+                "azure",
+                "--ray-address",
+                "127.0.0.1",
+            ]
+            result = runner.invoke(
+                app,
+                args,
+                input="https://foo.bar\n",
+            )
+            self.assertEqual(1, result.exit_code, result.stdout)
+
+    @patch("surfer.storage.enabled_providers", new=[])
+    def test_storage_provider_azure__provider_not_enabled(self):
+        with NamedTemporaryFile() as tmp:
+            # Run command
+            args = [
+                "init",
+                tmp.name,
+                "--storage-provider",
+                "azure",
+                "--ray-address",
+                "127.0.0.1",
+            ]
+            result = runner.invoke(
+                app,
+                args,
+                input="https://myaccount.blob.core.windows.net/pictures\n",
+            )
+            self.assertEqual(1, result.exit_code, result.stdout)
+
+    @patch.object(SurferConfigManager, "config_exists", return_value=False)
+    @patch.object(SurferConfigManager, "save_config")
+    def test_storage_provider_azure__valid_url(
+        self,
+        *_,
+    ):
+        with NamedTemporaryFile() as tmp:
+            # Run command
+            args = [
+                "init",
+                tmp.name,
+                "--storage-provider",
+                "azure",
+                "--ray-address",
+                "http://127.0.0.1",
+            ]
+            result = runner.invoke(
+                app,
+                args,
+                input="https://myaccount.blob.core.windows.net/pictures\n",
+            )
+            self.assertEqual(0, result.exit_code, result.stdout)
+
+    @patch("surfer.storage.enabled_providers", new=[])
+    def test_storage_provider_gcp__provider_not_enabled(self):
+        with NamedTemporaryFile() as tmp:
+            # Run command
+            args = [
+                "init",
+                tmp.name,
+                "--storage-provider",
+                "gcp",
+            ]
+            result = runner.invoke(
+                app,
+                args,
+            )
+            self.assertEqual(1, result.exit_code, result.stdout)
+
+    @patch("surfer.storage.enabled_providers", new=[])
+    def test_storage_provider_aws__provider_not_enabled(self):
+        with NamedTemporaryFile() as tmp:
+            # Run command
+            args = [
+                "init",
+                tmp.name,
+                "--storage-provider",
+                "aws",
+            ]
+            result = runner.invoke(
+                app,
+                args,
+            )
+            self.assertEqual(1, result.exit_code, result.stdout)
+
+    @patch.object(SurferConfigManager, "config_exists", return_value=True)
+    @patch.object(SurferConfigManager, "save_config")
+    def test_config_exists_should_ask_confirm(
+        self,
+        *_,
+    ):
+        with NamedTemporaryFile() as tmp:
+            args = [
+                "init",
+                tmp.name,
+                "--storage-provider",
+                "azure",
+                "--ray-address",
+                "http://127.0.0.1",
+            ]
+            # Run command without confirm -- should exit with 1
+            result = runner.invoke(
+                app,
+                args,
+                input="https://myaccount.blob.core.windows.net/p\n",
+            )
+            self.assertEqual(1, result.exit_code, result.stdout)
+
+            # Run command with confirm -- should exit with 0
+            result = runner.invoke(
+                app,
+                args,
+                input="https://myaccount.blob.core.windows.net/p\ny\n",
+            )
+            self.assertEqual(0, result.exit_code, result.stdout)
