56de0607f2ad1eccd843505930ed039d09d6d754
Michele Zanotti <m.zanotti019@gmail.com>
2023-03-20 18:09:21 +0100

    Add evaluate_model method to ModelEvaluator, update docstrings

--- apps/accelerate/cloud_surfer/surfer/common/models.py
+++ apps/accelerate/cloud_surfer/surfer/common/models.py
@@ -1,5 +1,5 @@
 import abc
-from typing import Iterable, Callable, Any
+from typing import Iterable, Callable, Any, Optional, Dict, Sequence
 
 
 class DataLoader(abc.ABC):
@@ -16,8 +16,40 @@ class ModelLoader(abc.ABC):
 
 class ModelEvaluator(abc.ABC):
     @abc.abstractmethod
-    def get_precision_metric_fn(self) -> Callable[[Any, Any, Any], float]:
+    def get_precision_metric_fn(self) -> Optional[Callable[[Any, Any, Any], float]]:
         """
         Load the metric to be used for accepting or refusing
         a precision-reduction optimization proposal.
+
+        The returned function must accept as inputs two tuples of tensors
+        (produced by the baseline and the optimized model) and the related
+        original labels, and it must return a float corresponding
+        to the metric value.
+
+        If None is returned, the metric
+        `nebullvm.measure.compute_relative_difference` will be used as default.
+        """
+
+    @abc.abstractmethod
+    def evaluate_model(
+        self,
+        model: Any,
+        input_data: Sequence,
+    ) -> Dict[str, float]:
+        """
+        Compute custom metrics on the given optimized model. These metrics will
+        be included in the experiment results.
+
+        The function must return a dictionary containing the metrics, where
+        each key represents the metric name.
+
+        The returned dictionary can be empty. If so, no additional metrics
+        will be included in the experiment output.
+
+        Parameters
+        ----------
+        model : Any
+            The optimized model to evaluate.
+        input_data: Sequence
+            The input data to be used for evaluating the model.
         """
